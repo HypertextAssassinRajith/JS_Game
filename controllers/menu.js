0fe7bae2036887c46df0fd1161e85dda23872ef0
Rajith Sanjaya <sanjayasenanayaka11@gmail.com>
2022-06-16 14:08:38 +0530

    ui changes

--- controllers/menu.js
+++ controllers/menu.js
@@ -9,7 +9,7 @@ $(document).keydown(function (event) {
             $("#support").addClass("selected");
         }else if ($("#support").hasClass("selected")){
             $("#support").removeClass("selected");
-            $("#quite").addClass("selected");
+            $("#quit").addClass("selected");
         }
     }
     else if (event.key === "ArrowUp"){
@@ -19,12 +19,22 @@ $(document).keydown(function (event) {
         }else if ($("#support").hasClass("selected")){
             $("#support").removeClass("selected");
             $("#option").addClass("selected");
-        }else if ($("#quite").hasClass("selected")){
-            $("#quite").removeClass("selected");
+        }else if ($("#quit").hasClass("selected")){
+            $("#quit").removeClass("selected");
             $("#support").addClass("selected");
         }
     }
 })
 
+
+
+$(".button-glow").mouseenter(function () {
+    $( "#start" ).removeClass( "selected" );
+    $(this).addClass("selected");
+
+});
+$(".button-glow").mouseleave(function () {
+    $(this).removeClass("selected")
+});
 
 
